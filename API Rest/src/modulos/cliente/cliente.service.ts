41c4028604222e2e58258b479089e0c611586122
Alberto Barrientos Davila <37272848+ABD2k@users.noreply.github.com>
2022-12-09 08:16:03 -0600

    final

--- API Rest/src/modulos/cliente/cliente.service.ts
+++ API Rest/src/modulos/cliente/cliente.service.ts
@@ -1,6 +1,9 @@
+import { PagoUserDto } from './dto/pago-user.dto';
+import { Request } from 'express';
+import { SaldoUserDto } from './dto/saldo-user.dto';
 import { LoginDTO } from './dto/login.dto';
-import { Clientes } from './cliente.entity';
-import { HttpException, Injectable } from '@nestjs/common';
+import { Clientes, Pagos, MetodosPago } from './cliente.entity';
+import { HttpException, Injectable, Req } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
@@ -8,7 +11,7 @@ var crypto = require('crypto');
 
 @Injectable()
 export class ClienteService {
-    constructor(@InjectRepository(Clientes) private clienteRepo:Repository<Clientes>,
+    constructor(@InjectRepository(Clientes) private clienteRepo:Repository<Clientes>,@InjectRepository(Pagos) private pagoRepo:Repository<Pagos>,
     private jwtService: JwtService) {}
 
     async login(cliente:LoginDTO){
@@ -20,12 +23,63 @@ export class ClienteService {
             id:userFind.id,
             email:userFind.email,
             nombre:userFind.nombre,
+            expiresIn : 3600,
+            timestamp : new Date().getTime()
         }
+        
         return {_token:this.jwtService.sign(data)};
     }
 
-    ver(){
-        return 'Hola';
+    async getSaldo(req:Request){
+    
+        const userFind = await this.clienteRepo.findOne({where:{id:req.body.cliente.id},relations:['paquete']});
+        //obtener cuantos meses han pasado desde la fecha de inicio del paquete
+        const meses = Math.floor((new Date().getTime() - new Date(userFind.fecha_creacion).getTime()) / (1000 * 60 * 60 * 24 * 30));
+        
+        return {
+            monto : userFind.paquete.precio * meses,
+            discount : userFind.descuento,
+            paquetNme : userFind.paquete.nombre_paquete  ,
+            paqueteDesc : userFind.paquete.detalles_paquete
+        }
+
+        
+        
     }
+
+    async setPago(pago:PagoUserDto, req:Request){
+        const userFind = await this.clienteRepo.findOne({where:{id:req.body.cliente.id},relations:['paquete']});
+        //obtener cuantos meses han pasado desde la fecha de inicio del paquete
+        const meses = Math.floor((new Date().getTime() - new Date(userFind.fecha_creacion).getTime()) / (1000 * 60 * 60 * 24 * 30));
+        const saldo = userFind.paquete.precio * meses;
+
+        var t = this.pagoRepo.save({
+            cliente:userFind,
+            fecha_pago:new Date(),
+            monto:saldo
+        }).then((res)=>{
+            this.clienteRepo.update(userFind.id,{fecha_creacion:new Date()});
+        }).catch((err)=>{
+            return {status:'ERROR'};
+        });
+        //
+        return {status:'OK'};
+
+    }
+
+    async getPlan (req:Request){
+        const userFind = await this.clienteRepo.findOne({where:{id:req.body.cliente.id},relations:['paquete']});
+        return {paquete:userFind.paquete, titular:req.body.cliente.nombre};
+    }
+
+    async getPagos(req:Request){
+        const userFind = await this.pagoRepo.find({where:{cliente:req.body.cliente.id}});
+        return userFind;
+    }
+
+    getFactura(req:Request){
+        
+    }
+
 
 }
